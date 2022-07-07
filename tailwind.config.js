4201bcb9d49a94eee00c4d9c384ce141a5ac383e
danny-jin <icarus96119@gmail.com>
2022-07-07 17:37:07 +0900

    feat: Import `redux`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+function range(start, end, increment = 1) {
+  const count = Math.floor((end - start + 1) / increment);
+  return Array(count).fill(0).map((_, idx) => start + idx * increment);
+}
+
+const minFontSize = 5;
+const maxFontSize = 140;
+
+const minSpacingPixel = 0;
+const maxSpacingPixel = 1000;
+const spacingPixelIncrement = 5;
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
